4bc9258ee16973b40750a5a7c81a565b3f3122d9
matt manes <mattmanes@pm.me>
2024-09-14 15:46:41 -0500

    chore: add to gitignore

--- src/test/java/GZDecompressorTest.java
+++ src/test/java/GZDecompressorTest.java
@@ -1,10 +1,32 @@
+import filehandler.GZDecompressor;
 import filehandler.Globber;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 
 public class GZDecompressorTest {
     public static void main(String[] args) {
         List<Path> paths = Globber.glob(ActivitiesDir.dir);
+        Path zipped = null;
+        // Find one that's a `.gz`
+        for (Path path : paths) {
+            if (path.toString().endsWith(".gz")) {
+                zipped = path;
+                break;
+            }
+        }
+        assert !(zipped == null);
+        byte[] unzipped = GZDecompressor.read(zipped);
+        assert unzipped.length > 0;
+        
+        // Write to file to that can be checked with FIT SDK.
+        Path outpath = Path.of("decompressTest.fit");
+        try (FileOutputStream out = new FileOutputStream(outpath.toFile())) {
+            out.write(unzipped);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
