b37de03a4aef73c19d6fa4413b5004bbf2b4e3fc
matt manes <mattmanes@pm.me>
2024-09-17 14:08:59 -0500

    feat: implement activity loader

---
+++ src/main/java/activityloader/ActivityLoader.java
@@ -0,0 +1,35 @@
+package activityloader;
+
+import filehandler.FITReader;
+import filehandler.FileType;
+import filehandler.GPXReader;
+import filehandler.Globber;
+import models.Activity;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActivityLoader {
+
+    public static Activity load(Path path) {
+        String name = path.toString();
+        if (FileType.isFITOrFITGZ(name)) {
+            return FITReader.parse(path);
+        } else if (FileType.isGPX(name)) {
+            return GPXReader.parse(path);
+        }
+        throw new IllegalArgumentException("No reader for " + name + " found.");
+    }
+
+    public static ArrayList<Activity> loadAll(Path dir) {
+        ArrayList<Activity> activities = new ArrayList<>();
+        List<Path> paths = Globber.glob(dir);
+        for (Path path : paths) {
+            activities.add(load(path));
+        }
+        // TODO: Add gps coordinate rounding
+        // TODO: Add gps boundary checking and toss those outside bounds
+        return activities;
+    }
+}
