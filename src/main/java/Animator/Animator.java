f1202c92b5cca4bcc8d8c0616c5f2ace0391471c
matt manes <mattmanes@pm.me>
2024-09-24 14:40:57 -0500

    refactor: break `animate` into smaller functions

--- src/main/java/Animator/Animator.java
+++ src/main/java/Animator/Animator.java
@@ -18,27 +18,64 @@ class RedComparator implements Comparator<Pixel> {
 
 public class Animator {
 
-    private static int sleepTime = 0;
+    private static final int sleepTime = Settings.animationFrameWait;
     private static int frameCount = 0;
+    private static final RedComparator redComparator = new RedComparator();
 
     public static void animate(HeatMapper heatmaps) throws InterruptedException {
-        Dimension screen = Settings.getCanvasSize();
-        StdDraw.setCanvasSize(screen.width, screen.height);
-        FrameGenerator frames = new FrameGenerator(heatmaps);
-        StdDraw.show(sleepTime);
+        setCanvasSize();
+        // Calling this to set `defer` to `true`, can't set it directly :/
+        show();
         StdDraw.setPenRadius(0.001);
-        for (ArrayList<Pixel> frame : frames) {
+        // Draw all frames
+        for (ArrayList<Pixel> frame : new FrameGenerator(heatmaps)) {
             ++frameCount;
-            StdDraw.clear(Color.BLACK);
-            drawFrame(frame);
-            StdDraw.show(sleepTime);
+            render(frame);
         }
     }
 
+    /**
+     * Clear the canvas, draw the frame, and then show it.
+     *
+     * @param frame The frame to render.
+     */
+    private static void render(ArrayList<Pixel> frame) {
+        clear();
+        drawFrame(frame);
+        show();
+    }
+
+
+    /**
+     * Show the current contents and set `defer` to `true`.
+     * <p>
+     * If `defer` is `false`, `StdDraw` will try to show each pixel as it is drawn,
+     * instead of the whole frame at once.
+     */
+    private static void show() {StdDraw.show(sleepTime);}
+
+    /**
+     * Set canvas size according to `Settings`.
+     */
+    private static void setCanvasSize() {
+        Dimension screen = Settings.getCanvasSize();
+        StdDraw.setCanvasSize(screen.width, screen.height);
+    }
+
+    /**
+     * Clear the canvas.
+     */
+    private static void clear() {StdDraw.clear(Color.BLACK);}
+
+    /**
+     * Draw a frame from a list of pixels.
+     *
+     * @param pixels The pixels to draw.
+     */
     private static void drawFrame(ArrayList<Pixel> pixels) {
         // Sort so that blue points are drawn first and red are drawn last.
         // This way red is more visible when there's overlap.
-        pixels.sort(new RedComparator());
+        pixels.sort(redComparator);
         for (Pixel pixel : pixels) {
             StdDraw.setPenColor(pixel.color());
             StdDraw.point(pixel.coordinate().x(), pixel.coordinate().y());
