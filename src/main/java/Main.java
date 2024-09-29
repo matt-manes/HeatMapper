7a2eec4018344386dcfaabb650bc1c1605d37586
matt manes <mattmanes@pm.me>
2024-09-29 13:10:28 -0500

    refactor: split main into smaller functions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,40 +28,53 @@ class Timer {
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println();
-        Path defaultData = Path.of(System.getProperty("user.dir") + Path.of("/data/activities"));
-        Path data = args.length > 0 ? Path.of(args[0]) : defaultData;
+    private static final Timer timer = new Timer();
 
-        Timer timer = new Timer();
-
+    private static ArrayList<Activity> load(Path data) {
+        timer.reset();
         System.out.println("Loading data from '" + data + "'...");
         ArrayList<Activity> activities = ActivityLoader.loadAll(data);
         System.out.println("Data loaded successfully.");
         System.out.println("Loaded " + activities.size() + " activities.");
         timer.lap();
+        return activities;
+    }
 
+    private static ArrayList<Activity> doPreprocessing(ArrayList<Activity> activities) {
+        timer.reset();
         System.out.println("Preprocessing data...");
         activities = Preprocessor.process(activities);
         System.out.println("Data set now has " + activities.size() + " valid activities.");
         timer.lap();
+        return activities;
+    }
 
+    private static ArrayList<Activity> sort(ArrayList<Activity> activities) {
+        timer.reset();
         System.out.println("Sorting activites by date...");
         activities = new ActivitySorter().sort(activities);
         System.out.println("Sorting complete.");
         timer.lap();
+        return activities;
+    }
 
-        System.out.println("Generating heat maps...");
-        HeatMapper heatmaps = new HeatMapper(activities);
-        System.out.println("Generation complete.");
-        timer.lap();
-
+    private static void animate(ArrayList<Activity> activities) {
+        timer.reset();
         System.out.println("Beginning animation...");
         try {
-            Animator.animate(heatmaps);
+            Animator.animate(new HeatMapper(activities));
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
         timer.lap();
+    }
+
+    public static void main(String[] args) {
+        Timer mainTimer = new Timer();
+        System.out.println();
+        Path defaultData = Path.of(System.getProperty("user.dir") + Path.of("/data/activities"));
+        Path data = args.length > 0 ? Path.of(args[0]) : defaultData;
+        animate(sort(doPreprocessing(load(data))));
+        mainTimer.lap();
     }
 }
