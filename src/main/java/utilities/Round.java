e7bf0014ef003be490c49fc23b0354891a82150e
matt manes <mattmanes@pm.me>
2024-10-29 15:49:37 -0500

    fix: fix near zero values returned when rounding value is too big

--- src/main/java/utilities/Round.java
+++ src/main/java/utilities/Round.java
@@ -5,12 +5,16 @@ public class Round {
      * Round `num` to the given number of places.
      *
      * @param num    The number to round.
-     * @param places Exception thrown if this is less than 1.
+     * @param places Exception thrown if this is less than 0.
      * @return The rounded number.
      */
     public static double round(double num, int places) {
         if (places < 0) throw new IllegalArgumentException("`places` must be positive.");
         double scale = Math.pow(10, places);
+        double tmp = num * scale;
+        // If `tmp` is out of this range and passed to `Math.round`
+        // we get either `MIN_VALUE` or `MAX_VALUE` divided by scale back.
+        if (tmp <= Long.MIN_VALUE || Long.MAX_VALUE <= tmp) return num;
         return Math.round(num * scale) / scale;
     }
 }
