e9aeb21190db43f8331fc43465bb488e9e08cf0f
matt manes <mattmanes@pm.me>
2024-11-08 18:15:48 -0600

    docs: add javadoc

--- src/main/java/utilities/Blend.java
+++ src/main/java/utilities/Blend.java
@@ -1,6 +1,14 @@
 package utilities;
 
 public class Blend {
+    /**
+     * Blends between two values according to a control value.
+     *
+     * @param a       The value when `control` is 0.
+     * @param b       The value when `control` is 1.
+     * @param control The control value, excpected to be between 0 and 1.
+     * @return The blended value.
+     */
     public static double blend(double a, double b, double control) {
         if (a == b || control == 0) return a;
         if (control == 1) return b;
